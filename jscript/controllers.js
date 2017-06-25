d6b8567a410c30197cf01c5544192c7cc27c36e2
grishma <atanur.grishma@gmail.com>
2017-06-25 13:40:40 +0530

    Add Contact button working

--- jscript/controllers.js
+++ jscript/controllers.js
@@ -1,43 +1,47 @@
-var myApp= angular.module('myApp', ['ngRoute']);
-myApp.config(function($routeProvider) {
-	$routeProvider
-		.when('/homePage', {
-			templateUrl: 'homePage.html',
-			controller: 'MyController'
-		})
-		.when('/editContact', {
-			templateUrl: 'editContact.html',
-			controller: 'MyController'
-		})
-		.otherwise({
-			redirectTo: '/homePage'
-		});
+var myApp = angular.module('myApp', ['ngRoute']);
+myApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/homePage', {
+            templateUrl: 'homePage.html',
+            controller: 'MyController'
+        })
+        .when('/editContact', {
+            templateUrl: 'editContact.html',
+            controller: 'MyController'
+        })
+        .when('/addContact', {
+            templateUrl: 'addContact.html',
+            controller: 'MyController'
+        })
+        .otherwise({
+            redirectTo: '/homePage'
+        });
 });
 
 
-myApp.controller('MyController',['$scope', function MyController($scope) {
-  $scope.contacts= [
-  {
-    "name":"Barot Bellingham",
-   "phone":"122345666",
-   "Email-id":"Barot@gmail.com" 
-  },
-  {
-    "name":"grishma",
-   "phone":"9535469419",
-   "Email-id":"grishma@gmail.com" 
-  },
-  {
-    "name":"core scientist",
-   "phone":"9742399049",
-   "Email-id":"corescientist@gmail.com" 
-  },
-  {
-    "name":"neha",
-   "phone":"677987698",
-   "Email-id":"neha@gmail.com" 
-  },
- 
-]
+myApp.controller('MyController', ['$scope', function MyController($scope) {
+    $scope.contacts = [
+        {
+            "name": "Barot Bellingham",
+            "phone": "122345666",
+            "Email-id": "Barot@gmail.com"
+        },
+        {
+            "name": "grishma",
+            "phone": "9535469419",
+            "Email-id": "grishma@gmail.com"
+        },
+        {
+            "name": "core scientist",
+            "phone": "9742399049",
+            "Email-id": "corescientist@gmail.com"
+        },
+        {
+            "name": "neha",
+            "phone": "677987698",
+            "Email-id": "neha@gmail.com"
+        },
+
+    ]
 }]);
 
